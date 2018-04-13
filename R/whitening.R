38fd43224d5850fca6f37a80b712b7027c86dc6b
Marie Perrot-Dockès <marie.perrocks@gmail.com>
2018-04-13 16:05:52 +0000

    version 1.1.2

--- R/whitening.R
+++ R/whitening.R
@@ -1,74 +1,71 @@
-whitening <-
-function(residuals,typeDep,pAR=1,qMA=0)
-{
-  n=dim(residuals)[1]
-  q=dim(residuals)[2]
-  
-  if (typeDep=="no_whitening")
-  {
-    return (Diagonal(q,1))
-  } 
-  
-  if (typeDep=="AR1")
-  {
-    phi_hat=c()
-    for (i in 1:n)
-    {
-      phi_hat[i]=arima(residuals[i,],order=c(1,0,0))$coef[1]
-    }
-    phi_hat_final=mean(phi_hat)
-    
-    phi_hat_vect=rep(-phi_hat_final,(q-1))
-    square_root_inv_hat_Sigma=bandSparse(q,k=c(1,0),diagonals=list(phi_hat_vect,c(sqrt(1-phi_hat_final^2),rep(1,(q-1)))))
-    return (square_root_inv_hat_Sigma)  
+#' This function provides an estimation of the inverse of the square root of the covariance matrix of each row of the residuals matrix.
+#'
+#' @param  residuals the residuals matrix obtained by fitting a linear model to each column of the response matrix as if they were independent
+#' @param  typeDep character in c("AR1", "ARMA", "nonparam") defining which type of dependence to use
+#' @param  pAR numerical, only use if typeDep = "ARMA", the parameter p for the ARMA(p, q) process
+#' @param  qMA numerical, only use if typeDep = "ARMA", the parameter q for the ARMA(p, q) process
+#' @return It returns the estimation of the inverse of the square root of the covariance matrix of each row of the residuals matrix.
+#' @examples
+#' data(copals_camera)
+#' Y=scale(Y[,1:100])
+#' X <- model.matrix( ~ group + 0)
+#' residuals=lm(as.matrix(Y)~X-1)$residuals
+#' whitening(residuals, "AR1")
+#' @export
+whitening <- function (residuals, typeDep, pAR = 1, qMA = 0){
+  n = dim(residuals)[1]
+  q = dim(residuals)[2]
+  if (typeDep == "no_whitening") {
+    return(Diagonal(q, 1))
   }
-  
-  if (typeDep=="ARMA")
-  {
-    phi_hat=matrix(0,n,max(pAR,1));theta_hat=matrix(0,n,max(qMA,1))
-    if ((pAR>=1)&&(qMA>=1))
-    {
-      for (i in 1:n)
-      {
-        phi_hat[i,]=arima(residuals[i,],order=c(pAR,0,qMA))$coef[1:pAR]
-        theta_hat[i,]=arima(residuals[i,],order=c(pAR,0,qMA))$coef[(pAR+1):(pAR+qMA)]
-      }
-    }
-    if ((pAR>=1)&&(qMA==0))
-    {
-      for (i in 1:n)
-      {
-        phi_hat[i,]=arima(residuals[i,],order=c(pAR,0,qMA))$coef[1:pAR]
-      }
-    }
-    if ((pAR==0)&&(qMA>=1))
-    {
-      for (i in 1:n)
-      {
-        theta_hat[i,]=arima(residuals[i,],order=c(pAR,0,qMA))$coef[(pAR+1):(pAR+qMA)]
-      }
-    }
-    phi_hat_final=colMeans(phi_hat);theta_hat_final=colMeans(theta_hat)
-  
-    acf_theo_hat=ARMAacf(ar=phi_hat_final,ma=theta_hat_final,lag.max=(q-1))  ### autocorrelation function
-    psi_hat=ARMAtoMA(ar=phi_hat_final,ma=theta_hat_final,1000)  ### computation of the (psi_k)_{k>=1} coefficients to compute the variance : 1+sum(psi_k^2)
-    variance_hat=1+sum(psi_hat^2)
-    Sigma_hat=toeplitz(acf_theo_hat)*variance_hat
-    square_root_inv_hat_Sigma=Matrix(round(solve(chol(Sigma_hat)),digits=6))
-    return (square_root_inv_hat_Sigma)
+  if (typeDep == "AR1") {
+    phi_hat <- t(apply(residuals, 1, function(x){
+      arima(x, order = c(1, 0, 0))$coef[1]
+    }))
+    phi_hat_final <- mean(phi_hat)
+    phi_hat_vect <- rep( - phi_hat_final, (q - 1))
+    square_root_inv_hat_Sigma <- bandSparse(q, k = c(1, 0),
+                                            diagonals = list(phi_hat_vect,
+                                                             c(sqrt(1 - phi_hat_final^2),
+                                                               rep(1, (q - 1)))
+                                            )
+    )
+    return(square_root_inv_hat_Sigma)
   }
-  
-  if(typeDep=="nonparam")
-  {   
-    vector_cov=matrix(0,n,q)
-    for (i in 1:n)
-    {
-      vector_cov[i,]=acf(residuals[i,],type="covariance",plot=FALSE,lag.max=(q-1))$acf
+  if (typeDep == "ARMA") {
+    Coefs <- apply(residuals, 1, function(x){
+      arima(x, order = c(pAR, 0, qMA))$coef
+    })
+    if (pAR > 1) { phi_hat_final <-  rowMeans(Coefs[1:pAR, ])
+    }else{
+      phi_hat_final <-  ifelse(pAR == 0, 0, mean(Coefs[1:pAR, ]))
     }
-    vector_cov_estim=colMeans(vector_cov)
-    cov_matrix=toeplitz(vector_cov_estim)
-    square_root_inv_hat_Sigma=Matrix(round(solve(chol(cov_matrix)),digits=6))
-    return (square_root_inv_hat_Sigma)
+
+    if (qMA > 1) { theta_hat_final <-  rowMeans(Coefs[(pAR + 1):(pAR + qMA), ])
+    }else{
+      theta_hat_final <-  ifelse(qMA == 0, 0, mean(Coefs[(pAR + 1):(pAR + qMA), ]))
+    }
+
+    acf_theo_hat <- ARMAacf(ar = phi_hat_final, ma = theta_hat_final,
+                            lag.max = (q - 1))
+    psi_hat <- ARMAtoMA(ar = phi_hat_final,
+                        ma = theta_hat_final, 1000)
+    variance_hat <- 1 + sum(psi_hat ^ 2)
+    Sigma_hat <- toeplitz(acf_theo_hat) * variance_hat
+    square_root_inv_hat_Sigma <- Matrix(round(solve(chol(Sigma_hat)),
+                                              digits = 6))
+    return(square_root_inv_hat_Sigma)
   }
-  
+  if (typeDep == "nonparam") {
+    vector_cov <- t(apply(residuals, 1, function(x){
+      acf(x, type = "covariance",
+          plot = FALSE, lag.max = (q - 1))$acf
+    }))
+
+    vector_cov_estim <- colMeans(vector_cov)
+    cov_matrix <- toeplitz(vector_cov_estim)
+    square_root_inv_hat_Sigma <- Matrix(round(solve(chol(cov_matrix)),
+                                              digits = 6))
+    return(square_root_inv_hat_Sigma)
+  }
 }
--- R/whitening_choice.R
+++ R/whitening_choice.R
@@ -1,20 +1,35 @@
-whitening_choice <-
-function (residuals,typeDeps='AR1',pAR=1,qMA=0)
+#' This function helps to choose the best whitening strategy among the following types of dependence
+#' modellings: AR1, ARMA, non parametric and without any whitening.
+#'
+#' @param  residuals the residuals matrix obtained by fitting a linear model to each column of the response matrix as if they were independent
+#' @param  typeDeps character in c("AR1", "ARMA", "nonparam", "no_whitening") defining which dependence structure to use to whiten the residuals.
+#' @param  pAR numerical, only use if typeDep = "ARMA", the parameter p for the ARMA(p, q) process
+#' @param  qMA numerical, only use if typeDep = "ARMA", the parameter q for the ARMA(p, q) process
+#' @return It provides a table giving the p-values for the different whitening tests applied to the residuals multiplied on the right
+#' by the inverse of the square root of the estimated covariance matrix.
+#' If the p-value is small (in general smaller than 0.05)
+#' it means that the hypothesis that each row of the  residuals "whitened" matrix is a white noise, is rejected.
+#' @examples
+#' data(copals_camera)
+#' Y=scale(Y[,1:100])
+#' X <- model.matrix( ~ group + 0)
+#' residuals=lm(as.matrix(Y)~X-1)$residuals
+#' whitening_choice(residuals,c("AR1","nonparam","ARMA","no_whitening"),
+#' pAR=1,qMA=1)
+#' @export
+whitening_choice <- function (residuals, typeDeps = "AR1", pAR = 1, qMA = 0)
 {
-  get_pvalue <- function(typeDep){
-    
-    
-    square_root_inv_hat_Sigma=whitening(residuals,typeDep,pAR=pAR,qMA=qMA)
-    whitened_residuals=residuals%*%square_root_inv_hat_Sigma
-    pvalue=whitening_test(whitened_residuals)
+  get_pvalue <- function(typeDep) {
+    square_root_inv_hat_Sigma = whitening(residuals, typeDep,
+                                          pAR = pAR, qMA = qMA)
+    whitened_residuals = residuals %*% square_root_inv_hat_Sigma
+    pvalue = whitening_test(whitened_residuals)
     return(pvalue)
   }
-  
-  Pvals<-sapply(typeDeps,get_pvalue)
-  Decision<-ifelse(Pvals<0.05,"NO WHITE NOISE","WHITE NOISE")
-  names(Pvals)[which(names(Pvals)=='ARMA')]<-paste('ARMA',pAR,qMA,sep=' ')
-  Result<-as.data.frame(cbind(Pvalue=round(Pvals,3),Decision))
-  
+  Pvals <- sapply(typeDeps, get_pvalue)
+  Decision <- ifelse(Pvals < 0.05, "NO WHITE NOISE", "WHITE NOISE")
+  names(Pvals)[which(names(Pvals) == "ARMA")] <- paste("ARMA",
+                                                       pAR, qMA, sep = " ")
+  Result <- as.data.frame(cbind(Pvalue = round(Pvals, 3), Decision))
   return(Result)
-  
 }
